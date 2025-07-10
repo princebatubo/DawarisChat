ced898a932366cc2d6af1c9f28bfb551306f9a1d
princebatubo <pdawaribatubo@gmail.com>
2025-07-10 10:14:43 +0100

    refactor: rebrand ChatwootApp module to DawarisChatApp

--- lib/chatwoot_app.rb
+++ lib/chatwoot_app.rb
@@ -2,7 +2,7 @@
 
 require 'pathname'
 
-module ChatwootApp
+module DawarisChatApp
   def self.root
     Pathname.new(File.expand_path('..', __dir__))
   end
@@ -17,7 +17,7 @@ module ChatwootApp
     @enterprise ||= root.join('enterprise').exist?
   end
 
-  def self.chatwoot_cloud?
+  def self.cloud_deployment?
     enterprise? && GlobalConfig.get_value('DEPLOYMENT_ENV') == 'cloud'
   end
 
