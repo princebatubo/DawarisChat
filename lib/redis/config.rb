c6242875b0e95cc282058439ed3a16921b0b78d2
princebatubo <pdawaribatubo@gmail.com>
2025-07-12 04:44:26 +0100

    git add lib/redis/config.rb git commit -m "Rewrite Redis::Config for production compatibility with Upstash and fix SSL verify_mode" git push origin develop

--- lib/redis/config.rb
+++ lib/redis/config.rb
@@ -1,49 +1,52 @@
-module Redis::Config
-  DEFAULT_SENTINEL_PORT ||= '26379'.freeze
-  class << self
-    def app
-      config
-    end
+# frozen_string_literal: true
 
-    def config
-      @config ||= sentinel? ? sentinel_config : base_config
-    end
+module Redis
+  class Config
+    DEFAULT_SENTINEL_PORT = '26379'.freeze
 
-    def base_config
-      {
-        url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379'),
-        password: ENV.fetch('REDIS_PASSWORD', nil).presence,
-        ssl_params: { verify_mode: Chatwoot.redis_ssl_verify_mode },
-        reconnect_attempts: 2,
-        timeout: 1
-      }
-    end
-
-    def sentinel?
-      ENV.fetch('REDIS_SENTINELS', nil).presence
-    end
-
-    def sentinel_url_config(sentinel_url)
-      host, port = sentinel_url.split(':').map(&:strip)
-      sentinel_url_config = { host: host, port: port || DEFAULT_SENTINEL_PORT }
-      password = ENV.fetch('REDIS_SENTINEL_PASSWORD', base_config[:password])
-      sentinel_url_config[:password] = password if password.present?
-      sentinel_url_config
-    end
-
-    def sentinel_config
-      redis_sentinels = ENV.fetch('REDIS_SENTINELS', nil)
-
-      # expected format for REDIS_SENTINELS url string is host1:port1, host2:port2
-      sentinels = redis_sentinels.split(',').map do |sentinel_url|
-        sentinel_url_config(sentinel_url)
+    class << self
+      def app
+        config
       end
 
-      # over-write redis url as redis://:<your-redis-password>@<master-name>/ when using sentinel
-      # more at https://github.com/redis/redis-rb/issues/531#issuecomment-263501322
-      master = "redis://#{ENV.fetch('REDIS_SENTINEL_MASTER_NAME', 'mymaster')}"
+      def config
+        @config ||= sentinel? ? sentinel_config : base_config
+      end
 
-      base_config.merge({ url: master, sentinels: sentinels })
+      def base_config
+        {
+          url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379'),
+          password: ENV.fetch('REDIS_PASSWORD', nil).presence,
+          ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
+          reconnect_attempts: 2,
+          timeout: 1
+        }
+      end
+
+      def sentinel?
+        ENV.fetch('REDIS_SENTINELS', nil).present?
+      end
+
+      def sentinel_url_config(sentinel_url)
+        host, port = sentinel_url.split(':').map(&:strip)
+        config = { host: host, port: port || DEFAULT_SENTINEL_PORT }
+
+        password = ENV.fetch('REDIS_SENTINEL_PASSWORD', base_config[:password])
+        config[:password] = password if password.present?
+
+        config
+      end
+
+      def sentinel_config
+        redis_sentinels = ENV.fetch('REDIS_SENTINELS', '')
+
+        sentinels = redis_sentinels.split(',').map do |sentinel_url|
+          sentinel_url_config(sentinel_url)
+        end
+
+        master = "redis://#{ENV.fetch('REDIS_SENTINEL_MASTER_NAME', 'mymaster')}"
+        base_config.merge({ url: master, sentinels: sentinels })
+      end
     end
   end
 end
